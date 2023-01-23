8227d80b564cff7733c60e1419ddec85f5780f08
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2023-01-23 10:08:52 -0500

    onStart() created in event_handlers

---
+++ event_handlers.js
@@ -0,0 +1,10 @@
+window.addEventListener("load", function () {
+    //Handle when the whole page finishes loading
+    //Use this to "set up" the initial state of things;
+    //Often, this includes populating the actorList.
+    // A sample:
+    onStart();
+});
+function onStart() {
+  
+}
