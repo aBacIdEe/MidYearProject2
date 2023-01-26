a53111c36c88a28835d46baadcf1bae2053bd53b
seognaMcitpyrC@gmail.com <jerlee25@bergen.org>
2023-01-26 08:23:32 -0500

    fixed a bit of stuff with paths and going places

--- event_handlers.js
+++ event_handlers.js
@@ -29,9 +29,10 @@ document.addEventListener("keydown", function (event) {
     }
     if (event.key === "p") {
         actorList.addActor(new Wall(2,3));
-       
+        actorList.addActor(new Wall(2,0));
         actorList.addActor(new Wall(3,5));
         actorList.addActor(new Wall(4,2));
+        actorList.addActor(new Wall(2,4));
         actorList.addActor(new Wall(5,2));
         actorList.addActor(new Wall(5,5));
         actorList.addActor(new Wall(6,5));
@@ -39,6 +40,8 @@ document.addEventListener("keydown", function (event) {
         actorList.addActor(new Wall(3,1));
         actorList.addActor(new Wall(4,5));
         actorList.addActor(new Wall(0,6));
+        actorList.addActor(new Wall(1,6));
+       
         actorList.addActor(new Wall(7,6));
         let enemy = new WalkingEnemy(3,4);
         actorList.addActor(enemy);
